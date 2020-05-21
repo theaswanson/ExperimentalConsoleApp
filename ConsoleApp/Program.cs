eef29c1c576ec1cf8d93c7afe7dd2d479b51c7eb
Adam Swanson <aswanson@apterainc.com>
2020-05-21 08:28:22 -0400

    feat: add DragonFruit example

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,11 +2,17 @@
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// A simple command-line application using DragonFruit.
+        /// </summary>
+        /// <param name="number">The integer to print</param>
+        /// <param name="verbose">Flag to print verbose output</param>
+        static void Main(int number, bool verbose = false)
         {
-            Console.WriteLine("Hello World!");
+            var output = verbose ? $"The numeric representation of the value you have just entered is expressed with the following characters on your display: {number}" : $"Number: {number}";
+            Console.WriteLine(output);
         }
     }
 }
